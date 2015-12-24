3dd12f3dce3b715d03f9610e6d37176e446abb82
Nozd <prosvet_egor@mail.ru>
2015-12-24 22:58:33 +0300

    23.12.2015

---
+++ SmallManager/SmallManager/Calculation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallManager
+{
+    class Calculation
+    {
+        //Номер заказа
+        private string NumberOrder;
+        public string _NumberOrder { get { return NumberOrder; } set { NumberOrder = value; } }
+        //Проработка
+        private string Analysis;
+        public string _Analysis { get { return Analysis; } set { Analysis = value; } }
+        //Согласование
+        private string Adjustment;
+        public string _Adjustment { get { return Adjustment; } set { Adjustment = value; } }
+
+    }
+}
---
+++ SmallManager/SmallManager/Production.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallManager
+{
+    public enum stepOfProduct
+    {
+        Не_запущена_в_производство = 0,
+        В_производстве = 1,
+        Изготовлена = 2
+    } ;
+
+    class Production
+    {
+        //Номер заказа
+        private string NumberOrder;
+        public string _NumberOrder { get { return NumberOrder; } set { NumberOrder = value; } }
+        //Проработка
+        private string Analysis;
+        public string _Analysis { get { return Analysis; } set { Analysis = value; } }
+        //Согласование
+        private string Adjustment;
+        public string _Adjustment { get { return Adjustment; } set { Adjustment = value; } }
+        //Заявка на материалы
+        private string RequestMaterials;
+        public string _RequestMaterials { get { return RequestMaterials; } set { RequestMaterials = value; } }
+        //Чертежи, развёртки
+        private string Drawing;
+        public string _Drawing { get { return Drawing; } set { Drawing = value; } }
+        //Программы
+        private string Program;
+        public string _Program { get { return Program; } set { Program = value; } }
+        //Изготовление
+        private stepOfProduct StepOfProduct;
+        public stepOfProduct _StepOfProduct { get { return StepOfProduct; } set { StepOfProduct = value; } }
+    }
+}
