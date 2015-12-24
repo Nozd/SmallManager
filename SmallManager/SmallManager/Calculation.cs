d624e3dd79c1b3614085015b1d1d097c335279a4
Nozd <prosvet_egor@mail.ru>
2015-12-25 01:14:20 +0300

    Добавлен ComboBox from Enum

--- SmallManager/SmallManager/Calculation.cs
+++ SmallManager/SmallManager/Calculation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +9,51 @@ namespace SmallManager
 {
     class Calculation
     {
-        //Номер заказа
-        private string NumberOrder;
-        public string _NumberOrder { get { return NumberOrder; } set { NumberOrder = value; } }
+        //Номер заявки
+        private string _NumberOrder;
+        public string NumberOrder { get { return _NumberOrder; } set { _NumberOrder = value; } }
         //Проработка
-        private string Analysis;
-        public string _Analysis { get { return Analysis; } set { Analysis = value; } }
+        private string _Analysis;
+        public string Analysis { get { return _Analysis; } set { _Analysis = value; } }
         //Согласование
-        private string Adjustment;
-        public string _Adjustment { get { return Adjustment; } set { Adjustment = value; } }
+        private string _Adjustment;
+        public string Adjustment { get { return _Adjustment; } set { _Adjustment = value; } }
+        //Коллекция CalculationList
+        private ObservableCollection<Calculation> _CalculationList = null;
+
+        public ObservableCollection<Calculation> CalculationList
+        {
+            get
+            {
+                if (_CalculationList != null)
+                {
+                    return _CalculationList;
+                }
+                //else
+                //{
+                    _CalculationList = new ObservableCollection<Calculation>();
+                    _CalculationList.Add(new Calculation()
+                    {
+                        NumberOrder = "10-Пр",
+                        Analysis = "Жук",
+                        Adjustment = "Отправлена 10.12.2015г."
+                    });
+                    _CalculationList.Add(new Calculation()
+                    {
+                        NumberOrder = "15-Пр",
+                        Analysis = "Беспалов А.В.",
+                        Adjustment = "Согласовано"
+                    });
+                    _CalculationList.Add(new Calculation()
+                    {
+                        NumberOrder = "441",
+                        Analysis = "Василькин",
+                        Adjustment = ""
+                    });
+                    return _CalculationList;
+                //}
+            }
+        }
 
     }
 }
--- SmallManager/SmallManager/Production.cs
+++ SmallManager/SmallManager/Production.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,26 +16,66 @@ namespace SmallManager
 
     class Production
     {
+        #region fields
         //Номер заказа
-        private string NumberOrder;
-        public string _NumberOrder { get { return NumberOrder; } set { NumberOrder = value; } }
+        private string _NumberOrder;
+        public string NumberOrder { get { return _NumberOrder; } set { _NumberOrder = value; } }
         //Проработка
-        private string Analysis;
-        public string _Analysis { get { return Analysis; } set { Analysis = value; } }
+        private string _Analysis;
+        public string Analysis { get { return _Analysis; } set { _Analysis = value; } }
         //Согласование
-        private string Adjustment;
-        public string _Adjustment { get { return Adjustment; } set { Adjustment = value; } }
+        private string _Adjustment;
+        public string Adjustment { get { return _Adjustment; } set { _Adjustment = value; } }
         //Заявка на материалы
-        private string RequestMaterials;
-        public string _RequestMaterials { get { return RequestMaterials; } set { RequestMaterials = value; } }
+        private string _RequestMaterials;
+        public string RequestMaterials { get { return _RequestMaterials; } set { _RequestMaterials = value; } }
         //Чертежи, развёртки
-        private string Drawing;
-        public string _Drawing { get { return Drawing; } set { Drawing = value; } }
+        private string _Drawing;
+        public string Drawing { get { return _Drawing; } set { _Drawing = value; } }
         //Программы
-        private string Program;
-        public string _Program { get { return Program; } set { Program = value; } }
+        private string _Program;
+        public string Program { get { return _Program; } set { _Program = value; } }
         //Изготовление
-        private stepOfProduct StepOfProduct;
-        public stepOfProduct _StepOfProduct { get { return StepOfProduct; } set { StepOfProduct = value; } }
+        private stepOfProduct _StepOfProduct;
+        public stepOfProduct StepOfProduct { get { return _StepOfProduct; } set { _StepOfProduct = value; } }
+        #endregion 
+        //Коллекция _ProductionList
+        private ObservableCollection<Production> _ProductionList = null;
+
+        public ObservableCollection<Production> ProductionList
+        {
+            get
+            {
+                if (_ProductionList != null)
+                {
+                    return _ProductionList;
+                }
+                else
+                {
+                    _ProductionList = new ObservableCollection<Production>();
+                    _ProductionList.Add(new Production()
+                    {
+                        _NumberOrder = "12",
+                        _Analysis = "Беспалов",
+                        _Adjustment = "Согласовано",
+                        _RequestMaterials = "Заявка у секретарю",
+                        _Drawing = "В локальной сети",
+                        _Program = "Программы отданы в цех",
+                        _StepOfProduct = stepOfProduct.В_производстве
+                    });
+                    _ProductionList.Add(new Production()
+                    {
+                        _NumberOrder = "152",
+                        _Analysis = "Беспалов, Жук",
+                        _Adjustment = "На согласовании",
+                        _RequestMaterials = "Не подавалась",
+                        _Drawing = "",
+                        _Program = "",
+                        _StepOfProduct = stepOfProduct.Не_запущена_в_производство
+                    });
+                    return _ProductionList;
+                }
+            }
+        }
     }
 }
